9657f26a8259e1f3fc4014ef553be431199f7271
Guru Dev <guru01803@gmail.com>
2023-10-11 17:54:57 +0530

    updated

--- lib/payment/payment.dart
+++ lib/payment/payment.dart
@@ -49,23 +49,50 @@ class _PaymentScreenState extends State<PaymentScreen> {
                   ListTile(
                     title: Text('Mobile Number: ${widget.mobileNumber}'),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PaymentSelectionScreen(
-                            username: widget.username,
-                            mobileNumber: widget.mobileNumber,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PaymentSelectionScreen(
+                              username: widget.username,
+                              mobileNumber: widget.mobileNumber,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                    child: Text('Continue'),
+                        );
+                      },
+                      child: Text('Continue'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 28, 30, 66),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              color: Color.fromARGB(255, 15, 17, 42),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Center(
+                        child: Icon(
+                      Icons.miscellaneous_services_outlined,
+                      size: 120,
+                    )),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
